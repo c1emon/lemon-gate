6e341ee14423d6b3978f0327ee9da6d51a9d433d
clemon <cjw7360chen@gmail.com>
2024-05-05 08:36:09 +0800

    style bug fix

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -10,18 +10,22 @@ const tabId = ref<number>(0)
 
 <template>
   <div :style="{ 'padding-top': `${getStatusBarHeight() + getNavBarHeight()}px`, 'height': `${getScreenHeight()}px`, 'background-image': 'url(https://pu61ic.oss-cn-shanghai.aliyuncs.com/watermark_ad8d1.png)' }" box-border w-full bg-cover p="x-4">
-    <div box-border h-100px w-full flex justify-around flex-items-center bg-transparent>
-      <wd-img
-        :width="70"
-        :height="70"
-        mode="aspectFit"
-        round
-        :src="peopleLogo"
-      />
-      <wd-tabs v-model="tabId" custom-class="tabs">
-        <wd-tab title="Login" />
-        <wd-tab title="Sign up" />
-      </wd-tabs>
+    <div box-border w-full flex justify-between flex-items-center bg-transparent p-b-40px>
+      <div w="55%" flex justify-start>
+        <wd-img
+          :width="70"
+          :height="70"
+          mode="aspectFit"
+          round
+          :src="peopleLogo"
+        />
+      </div>
+      <div w="40%">
+        <wd-tabs v-model="tabId" custom-class="tabs">
+          <wd-tab title="Login" />
+          <wd-tab title="Sign up" />
+        </wd-tabs>
+      </div>
     </div>
 
     <div v-if="tabId === 0">
@@ -74,20 +78,50 @@ const tabId = ref<number>(0)
         <wd-input placeholder=" " size="large" clearable no-border show-password b-rd-16px />
       </div>
     </div>
-    <div pos-fixed pos-bottom-none pos-left-none h-200px w-full b-rd-24px bg-white p="x-4px t-24px">
+    <div pos-fixed pos-bottom-none pos-left-none h-200px w-full b-rd-28px bg-white p="x-4px t-24px b-8px">
       <div w-full flex justify-around flex-items-center>
-        <div flex flex-col flex-items-start w="55%">
+        <div w="40%" flex flex-col flex-items-start>
           <div p-b-20px text-size-lg>
-            Or continue with:
+            Or continue with1:
           </div>
-          <div w-full flex flex-wrap justify-around flex-items-center>
+          <div flex flex-wrap justify-between flex-items-start>
             <wd-img
+              p-b-10px
               :width="40"
               :height="40"
               mode="aspectFit"
               :src="wxLogo"
             />
             <wd-img
+              p-b-10px
+              :width="40"
+              :height="40"
+              mode="aspectFit"
+              :src="zhifubaoLogo"
+            />
+            <wd-img
+              p-b-10px
+              :width="40"
+              :height="40"
+              mode="aspectFit"
+              :src="wxLogo"
+            />
+            <wd-img
+              p-b-10px
+              :width="40"
+              :height="40"
+              mode="aspectFit"
+              :src="zhifubaoLogo"
+            />
+            <wd-img
+              p-b-10px
+              :width="40"
+              :height="40"
+              mode="aspectFit"
+              :src="wxLogo"
+            />
+            <wd-img
+              p-b-10px
               :width="40"
               :height="40"
               mode="aspectFit"
