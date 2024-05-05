463c581fad8bacf9ff1e010f6387a6bb6105f438
clemon <cjw7360chen@gmail.com>
2024-05-05 11:15:52 +0800

    support force

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,11 +1,42 @@
 <!-- eslint-disable no-console -->
 <script setup lang="ts">
+import { useToast } from 'wot-design-uni'
 import { getNavBarHeight, getScreenHeight, getStatusBarHeight } from '~/composables/theme'
 import peopleLogo from '~/static/people.png'
 import wxLogo from '~/static/wx.png'
-import zhifubaoLogo from '~/static/zhifubao.png'
+import alipayLogo from '~/static/alipay.png'
+
+const toast = useToast()
 
 const tabId = ref<number>(0)
+const forceId = ref<number>(-1)
+const account = ref<string>('')
+const password = ref<string>('')
+
+const btnText = computed(() => {
+  return tabId.value === 0 ? 'Login' : 'Sign Up'
+})
+
+const isBadAccount = computed(() => {
+  if (account.value.length > 5)
+    return true
+  return false
+})
+
+function forgetPwd() {
+  toast.warning(`不好意思～忘记密码暂时未实现`)
+}
+
+function btnClk() {
+  if (tabId.value === 0)
+    toast.info(`登录...`)
+  else
+    toast.info(`注册...`)
+}
+
+function otherLogin(provider: string) {
+  toast.warning(`不好意思～第三方登录器${provider}暂时未实现`)
+}
 </script>
 
 <template>
@@ -39,17 +70,19 @@ const tabId = ref<number>(0)
         Account
       </div>
       <div p-b-16px>
-        <wd-input type="text" size="large" placeholder=" " no-border b-rd-16px />
+        <wd-input v-model="account" type="text" size="large" :error="isBadAccount" confirm-type="next" confirm-hold clearable placeholder=" " no-border b-rd-16px @confirm="() => forceId = 1" @focus="() => forceId = -1" />
       </div>
 
       <div p-b-6px text-left text-size-lg>
         Password
       </div>
       <div p-b-16px>
-        <wd-input placeholder=" " size="large" clearable no-border show-password b-rd-16px />
+        <wd-input v-model="password" placeholder=" " :focus="forceId === 1" size="large" clearable no-border show-password b-rd-16px />
       </div>
-      <div text-right text-size-base>
-        Forget?
+      <div flex justify-end>
+        <div text-size-base @click="forgetPwd">
+          Forget?
+        </div>
       </div>
     </div>
     <div v-else>
@@ -63,13 +96,13 @@ const tabId = ref<number>(0)
         Account Name
       </div>
       <div p-b-16px>
-        <wd-input type="text" size="large" placeholder=" " no-border b-rd-16px />
+        <wd-input type="text" size="large" placeholder=" " clearable no-border b-rd-16px />
       </div>
       <div p-b-6px text-left text-size-lg>
         Email or Phone
       </div>
       <div p-b-16px>
-        <wd-input type="text" size="large" placeholder=" " no-border b-rd-16px />
+        <wd-input type="text" size="large" placeholder=" " clearable no-border b-rd-16px />
       </div>
       <div p-b-6px text-left text-size-lg>
         Create Password
@@ -82,57 +115,31 @@ const tabId = ref<number>(0)
       <div w-full flex justify-around flex-items-center>
         <div w="40%" flex flex-col flex-items-start>
           <div p-b-20px text-size-lg>
-            Or continue with1:
+            Or continue with:
           </div>
           <div flex flex-wrap justify-between flex-items-start>
             <wd-img
-              p-b-10px
+              p-5px
               :width="40"
               :height="40"
               mode="aspectFit"
               :src="wxLogo"
+              @click="() => otherLogin('wx')"
             />
             <wd-img
-              p-b-10px
+              p-5px
               :width="40"
               :height="40"
               mode="aspectFit"
-              :src="zhifubaoLogo"
-            />
-            <wd-img
-              p-b-10px
-              :width="40"
-              :height="40"
-              mode="aspectFit"
-              :src="wxLogo"
-            />
-            <wd-img
-              p-b-10px
-              :width="40"
-              :height="40"
-              mode="aspectFit"
-              :src="zhifubaoLogo"
-            />
-            <wd-img
-              p-b-10px
-              :width="40"
-              :height="40"
-              mode="aspectFit"
-              :src="wxLogo"
-            />
-            <wd-img
-              p-b-10px
-              :width="40"
-              :height="40"
-              mode="aspectFit"
-              :src="zhifubaoLogo"
+              :src="alipayLogo"
+              @click="() => otherLogin('alipay')"
             />
           </div>
         </div>
 
         <div w="40%">
-          <wd-button hairline :block="true" icon="arrow-right1" size="large">
-            Go
+          <wd-button hairline :block="true" icon="arrow-right1" size="large" @click="btnClk">
+            {{ btnText }}
           </wd-button>
         </div>
       </div>
