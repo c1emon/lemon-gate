c2622a999260e07eb72bdfaa27f8b06f262bb7ca
clemon <cjw7360chen@gmail.com>
2024-05-06 00:44:49 +0800

    support phone login

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -9,12 +9,16 @@ import alipayLogo from '~/static/alipay.png'
 const toast = useToast()
 
 const tabId = ref<number>(0)
+const loginTypeId = ref<number>(0)
 const forceId = ref<number>(-1)
+
+const name = ref<string>('clemon')
 const account = ref<string>('')
 const password = ref<string>('')
+const emailOrPhone = ref<string>('')
 
 const btnText = computed(() => {
-  return tabId.value === 0 ? 'Login' : 'Sign Up'
+  return tabId.value === 0 ? '登录' : '注册'
 })
 
 const isBadAccount = computed(() => {
@@ -40,7 +44,7 @@ function otherLogin(provider: string) {
 </script>
 
 <template>
-  <div :style="{ 'padding-top': `${getStatusBarHeight() + getNavBarHeight()}px`, 'height': `${getScreenHeight()}px`, 'background-image': 'url(https://pu61ic.oss-cn-shanghai.aliyuncs.com/watermark_ad8d1.png)' }" box-border w-full bg-cover p="x-4">
+  <div :style="{ 'padding-top': `${getStatusBarHeight() + getNavBarHeight()}px`, 'height': `${getScreenHeight()}px`, 'background-image': 'url(https://oss.app.clemon.icu:883/static/imgs/watermark_ad8d1.png)' }" box-border w-full bg-cover p="x-4">
     <div box-border w-full flex justify-between flex-items-center bg-transparent p-b-40px>
       <div w="55%" flex justify-start>
         <wd-img
@@ -52,7 +56,7 @@ function otherLogin(provider: string) {
         />
       </div>
       <div w="40%">
-        <wd-tabs v-model="tabId" custom-class="tabs">
+        <wd-tabs v-model="tabId">
           <wd-tab title="Login" />
           <wd-tab title="Sign up" />
         </wd-tabs>
@@ -60,62 +64,85 @@ function otherLogin(provider: string) {
     </div>
 
     <div v-if="tabId === 0">
-      <div p-b-10px text-left text-size-4xl>
-        Welcome Back,
+      <div p-b-8px text-left text-size-4xl>
+        欢迎回来,
       </div>
-      <div p-b-24px text-left text-size-2xl>
-        Clemon.
+      <div p-b-10px text-left text-size-2xl>
+        {{ name }}.
       </div>
-      <div block p-b-6px text-left text-size-lg>
-        Account
-      </div>
-      <div p-b-16px>
-        <wd-input v-model="account" type="text" size="large" :error="isBadAccount" confirm-type="next" confirm-hold clearable placeholder=" " no-border b-rd-16px @confirm="() => forceId = 1" @focus="() => forceId = -1" />
-      </div>
-
-      <div p-b-6px text-left text-size-lg>
-        Password
-      </div>
-      <div p-b-16px>
-        <wd-input v-model="password" placeholder=" " :focus="forceId === 1" size="large" clearable no-border show-password b-rd-16px />
-      </div>
-      <div flex justify-end>
-        <div text-size-base @click="forgetPwd">
-          Forget?
-        </div>
-      </div>
+      <wd-tabs v-model="loginTypeId">
+        <wd-tab title="密码登录">
+          <div block p-b-6px p-t-6px text-left text-size-lg>
+            账号
+          </div>
+          <div p-b-16px>
+            <wd-input v-model="account" type="text" size="large" :error="isBadAccount" confirm-type="next" confirm-hold clearable placeholder=" " no-border b-rd-16px @confirm="() => forceId = 1" @focus="() => forceId = -1" />
+          </div>
+          <div p-b-6px text-left text-size-lg>
+            密码
+          </div>
+          <div p-b-16px>
+            <wd-input v-model="password" placeholder=" " :focus="forceId === 1" size="large" clearable no-border show-password b-rd-16px />
+          </div>
+          <div flex justify-end>
+            <div text-size-base @click="forgetPwd">
+              Forget?
+            </div>
+          </div>
+        </wd-tab>
+        <wd-tab title="手机登录">
+          <div block p-b-6px p-t-6px text-left text-size-lg>
+            手机号
+          </div>
+          <div p-b-16px>
+            <wd-input v-model="account" type="text" size="large" :error="isBadAccount" confirm-type="next" confirm-hold clearable placeholder=" " no-border b-rd-16px @confirm="() => forceId = 1" @focus="() => forceId = -1" />
+          </div>
+          <div p-b-6px text-left text-size-lg>
+            验证码
+          </div>
+          <div p-b-16px>
+            <wd-input v-model="password" placeholder=" " :focus="forceId === 1" size="large" no-border use-suffix-slot b-rd-16px>
+              <template #suffix>
+                <wd-button size="small">
+                  发送验证码
+                </wd-button>
+              </template>
+            </wd-input>
+          </div>
+        </wd-tab>
+      </wd-tabs>
     </div>
     <div v-else>
       <div p-b-10px text-left text-size-4xl>
-        Hello New,
+        你好 新朋友,
       </div>
       <div p-b-24px text-left text-size-2xl>
-        Let's make a new account
+        欢迎加入我们
       </div>
       <div p-b-6px text-left text-size-lg>
-        Account Name
+        昵称
       </div>
       <div p-b-16px>
-        <wd-input type="text" size="large" placeholder=" " clearable no-border b-rd-16px />
+        <wd-input v-model="account" type="text" size="large" placeholder=" " clearable confirm-type="next" confirm-hold no-border b-rd-16px @confirm="() => forceId = 2" @focus="() => forceId = -1" />
       </div>
       <div p-b-6px text-left text-size-lg>
-        Email or Phone
+        邮箱
       </div>
       <div p-b-16px>
-        <wd-input type="text" size="large" placeholder=" " clearable no-border b-rd-16px />
+        <wd-input v-model="emailOrPhone" type="text" size="large" placeholder=" " :focus="forceId === 2" confirm-type="next" confirm-hold clearable no-border b-rd-16px @confirm="() => forceId = 3" @focus="() => forceId = 2" />
       </div>
       <div p-b-6px text-left text-size-lg>
-        Create Password
+        创建密码
       </div>
       <div>
-        <wd-input placeholder=" " size="large" clearable no-border show-password b-rd-16px />
+        <wd-input v-model="password" placeholder=" " size="large" :focus="forceId === 3" clearable no-border show-password b-rd-16px />
       </div>
     </div>
     <div pos-fixed pos-bottom-none pos-left-none h-200px w-full b-rd-28px bg-white p="x-4px t-24px b-8px">
       <div w-full flex justify-around flex-items-center>
         <div w="40%" flex flex-col flex-items-start>
           <div p-b-20px text-size-lg>
-            Or continue with:
+            其他登录方式:
           </div>
           <div flex flex-wrap justify-between flex-items-start>
             <wd-img
